6bdf1dbabffe4c69c71fac06a85ff1af48617282
Andreas Fetzer <a.fetzer@dkfz-heidelberg.de>
2014-09-08 19:07:09 +0200

    reader for loading a dose file
    Conflicts:
    	Modules/DicomRT/CMakeLists.txt
    	Modules/DicomRT/Testing/files.cmake
    	Modules/DicomRT/files.cmake

--- Modules/DicomRT/files.cmake
+++ Modules/DicomRT/files.cmake
@@ -1,9 +1,22 @@
 SET(CPP_FILES
   mitkRTStructureSetReader.cpp
+  mitkRTDoseReader.cpp
+  DataStructures/mitkRTConstants.cpp
+  DataStructures/mitkIsoDoseLevel.cpp
+  DataStructures/mitkIsoDoseLevelCollections.cpp
+  DataStructures/mitkIsoDoseLevelSetProperty.cpp
+  DataStructures/mitkIsoDoseLevelVectorProperty.cpp
 )
 
 set(H_FILES
+  mitkRTDoseReader.h
   mitkRTStructureSetReader.h
+  DataStructures/mitkIsoDoseLevel.h
+  DataStructures/mitkIsoDoseLevelCollections.h
+  DataStructures/mitkIsoDoseLevelSetProperty.h
+  DataStructures/mitkIsoDoseLevelVectorProperty.h
+  DataStructures/mitkDoseValueType.h
+  DataStructures/mitkRTConstants.h
 )
 
 set(TPP_FILES
